a39c9834cc61451967e66404e65d15aae5bb5498
user3 <user3@163.com>
2014-10-10 10:59:21 +0800

    dasd

--- test02/src/com/example/test02/MainActivity.java
+++ test02/src/com/example/test02/MainActivity.java
@@ -10,12 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
-
+	private void click(){}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 		
 	}
 
